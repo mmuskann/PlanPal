e65a37d85339038ed53a87d1fd64e2c8e6779a90
N. Muskan <mmuskann2004@gmail.com>
2026-07-23 18:36:21 -0400

    fixed routing error

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom'
 function PostCard({ post }) {
     return (
         <div className="post-card">
-            <Link to={'/post/${post.id}'}>
+            <Link to={`/postDetail/${post.id}`}>
                 <p>Posted {post.created_at}</p>
-                <h2>post.title</h2>
-                <P>{post.upvotes} upvotes</P>
+                <h2>{post.title}</h2>
+                <p>{post.upvotes} upvotes</p>
             </Link>
         </div>
     )
