86daa0a24dacd23aa291b3540d8cd4c964e780a2
N. Muskan <mmuskann2004@gmail.com>
2026-07-23 14:30:58 -0400

    Added Supabase Database

---
+++ src/client.js
@@ -0,0 +1,12 @@
+import { createClient } from "@supabase/supabase-js"
+
+
+const URL = import.meta.env.URL
+
+const API_KEY = import.meta.env.API_KEY
+
+
+export const supabase = createClient(
+    URL,
+    API_KEY
+)
